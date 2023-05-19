da7bf144e4d97eca884a886742c525f6bc12d778
nasir ali <a.nasirali@outlook.com>
2023-05-19 23:35:19 +0100

    added 300 functionality

--- script.js
+++ script.js
@@ -40,9 +40,61 @@ document.addEventListener("DOMContentLoaded", function () {
 
   ///////////////////////// ABOVE IS NAV /////////////////////////
   ///////////////////////////////////////////////////////////////
+  //
+  //
+  //
+
+  ///////////////////////// BELOW IS Fetching Data  //////////////
+  ///////////////////////////////////////////////////////////////
 
   //
+  // Get all episodes from the episodes.js file
+  var allEpisodes = getAllEpisodes();
+  ///
+  async function fetchAllSeasons(showId) {
+    if (showId === "none") {
+      return [];
+    } else {
+      const response = await fetch(`https://api.tvmaze.com/shows/${showId}/seasons`);
+      const data = await response.json();
+      return data;
+    }
+  }
+
+  async function fetchShows() {
+    return fetch(`https://api.tvmaze.com/shows`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error fetching shows");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching shows:", error);
+      });
+  }
+
+  async function fetchAllEpisodesData(show) {
+    try {
+      const response = await fetch(`https://api.tvmaze.com/shows/${show.id}/episodes`);
+      if (response.ok) {
+        const episodesData = await response.json();
+        return episodesData;
+      } else {
+        throw new Error(`Error fetching episodes for show ID: ${show.id}`);
+      }
+    } catch (error) {
+      console.error("Error fetching episodes:", error);
+      return [];
+    }
+  }
   //
+  //
+
+  ///////////////////////// ABOVE IS Fetching Data ///////////////
+  ///////////////////////////////////////////////////////////////
+
   //
   // Create a container for the episodes
   var episodesContainer = document.createElement("div");
@@ -52,50 +104,88 @@ document.addEventListener("DOMContentLoaded", function () {
   //
   //
   //
-  // Get all episodes from the episodes.js file
-  var allEpisodes = getAllEpisodes();
-
-  //
-  //
-  //
-  // Function to create the dropdown select element
-
-  function createDropdownSelect() {
-    var dropdownSelect = document.createElement("select");
-    dropdownSelect.setAttribute("id", "dropdownSelect");
+  // Function to create the dropdown EPISODE select element
+  function createDropdownEpisodeSelect() {
+    var dropdownEpisodeSelect = document.createElement("select");
+    dropdownEpisodeSelect.setAttribute("id", "dropdownEpisodeSelect");
 
     // Add options to the dropdown select
     var defaultOption = document.createElement("option");
     defaultOption.textContent = "All Episodes";
-    dropdownSelect.appendChild(defaultOption);
+    dropdownEpisodeSelect.appendChild(defaultOption);
 
     // Loop through each episode and create an option for it
     allEpisodes.forEach(function (episode) {
       var option = document.createElement("option");
       option.textContent = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${episode.name}`;
       option.value = episode.id; // Assuming each episode has a unique ID
-      dropdownSelect.appendChild(option);
+      dropdownEpisodeSelect.appendChild(option);
     });
 
     // Event listener for dropdown select change
-    dropdownSelect.addEventListener("change", function () {
-      var selectedEpisodeId = dropdownSelect.value;
-      if (selectedEpisodeId === "all") {
+    dropdownEpisodeSelect.addEventListener("change", function () {
+      var selectedEpisodeId = dropdownEpisodeSelect.value;
+      if (selectedEpisodeId === "" || selectedEpisodeId === "All Episodes") {
         displayEpisodes(allEpisodes);
       } else {
-        var selectedEpisode = allEpisodes.find(function (episode) {
-          return episode.id === selectedEpisodeId;
-        });
+        var selectedEpisode = allEpisodes.find((episode) => episode.id === Number(selectedEpisodeId));
         displayEpisodes([selectedEpisode]);
       }
     });
 
-    return dropdownSelect;
+    return dropdownEpisodeSelect;
   }
 
   // Create the dropdown select element
-  var dropdownSelect = createDropdownSelect();
-  topBar.appendChild(dropdownSelect);
+  var dropdownEpisodeSelect = createDropdownEpisodeSelect();
+  topBar.appendChild(dropdownEpisodeSelect);
+
+  //
+  //
+  //
+  // Function to create the dropdown SHOW select element
+  // Define createDropdownShowSelect as an async function
+  async function createDropdownShowSelect() {
+    // Fetch shows data
+
+    const allShows = await fetchShows();
+
+    var dropdownShowSelect = document.createElement("select");
+    dropdownShowSelect.setAttribute("id", "dropdownShowSelect");
+
+    // Add options to the dropdown select
+    var defaultOption = document.createElement("option");
+    defaultOption.textContent = "All Shows";
+    dropdownShowSelect.appendChild(defaultOption);
+
+    // Loop through each show and create an option for it
+    allShows.forEach(function (show) {
+      var option = document.createElement("option");
+      option.textContent = `${show.name}`;
+      option.value = show.id; // Assuming each show has a unique ID
+      dropdownShowSelect.appendChild(option);
+    });
+
+    // Event listener for dropdown select change
+    dropdownShowSelect.addEventListener("change", function () {
+      var selectedShowId = dropdownShowSelect.value;
+
+      console.log("selectedShowId ", selectedShowId);
+      if (selectedShowId === "" || selectedEpisodeId === "All Shows") {
+        displayEpisodes(allEpisodes);
+      } else {
+        var selectedShow = allShows.find((show) => show.id === Number(selectedShowId));
+        displayEpisodes(selectedShow.episodes); // Pass the episodes array of the selected show
+      }
+    });
+
+    return dropdownShowSelect;
+  }
+
+  // Create the dropdown select element
+  createDropdownShowSelect().then(function (dropdownShowSelect) {
+    topBar.appendChild(dropdownShowSelect);
+  });
 
   ///////////////////////// BELOW IS FOOTER ///////////////////////
   ////////////////////////////////////////////////////////////////
@@ -143,17 +233,15 @@ document.addEventListener("DOMContentLoaded", function () {
   //
   // Function to display episodes
   function displayEpisodes(episodes) {
-    // Clear the previous episodes
-    episodesContainer.innerHTML = "";
-
-    //
-    //
     // Filter episodes based on search input
     var searchTerm = searchBar.value.toLowerCase();
     var filteredEpisodes = episodes.filter(function (episode) {
       var episodeName = episode.name.toLowerCase();
       return episodeName.includes(searchTerm);
     });
+
+    // Clear the previous episodes
+    episodesContainer.innerHTML = "";
 
     // Create grid for episodes
     var grid = document.createElement("div");
@@ -164,7 +252,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var totalEpisodes = filteredEpisodes.length;
 
     // Display 5 episodes across and 10 deep
-    var episodesPerPage = 16; // 3 (across) * 5 (deep)
+    var episodesPerPage = 16; // 4 (across) * 4 (deep)
     var totalPages = Math.ceil(totalEpisodes / episodesPerPage);
     // Get the current page number
     var currentPage = 1;
@@ -187,9 +275,9 @@ document.addEventListener("DOMContentLoaded", function () {
         episodeContainer.setAttribute("class", "episode");
 
         // Create the episode season element
-        var episodeName = document.createElement("h4");
-        episodeName.textContent = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-        episodeContainer.appendChild(episodeName);
+        var episodeSeason = document.createElement("h4");
+        episodeSeason.textContent = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+        episodeContainer.appendChild(episodeSeason);
 
         // Create the episode name element
         var episodeName = document.createElement("h3");
@@ -210,14 +298,70 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Append the episode container to the grid
         grid.appendChild(episodeContainer);
+        generatePagination(totalEpisodes);
       }
     }
 
     // Show the first page of episodes initially
     showEpisodes(currentPage);
+    generatePagination(totalPages);
+  }
+
+  //
+  //
+  //  displays Shows
+  // Function to display shows
+  function displayShows(shows) {
+    // Filter shows based on search input
+    var searchTerm = searchBar.value.toLowerCase();
+    var filteredShows = shows.filter(function (show) {
+      var showName = show.name.toLowerCase();
+      return showName.includes(searchTerm);
+    });
+
+    // Clear the previous shows
+    episodesContainer.innerHTML = "";
+
+    // Create grid for shows
+    var grid = document.createElement("div");
+    grid.setAttribute("class", "grid");
+    episodesContainer.appendChild(grid);
+
+    // Loop through the shows and create elements for each
+    filteredShows.forEach(function (show) {
+      // Create a container for each show
+      var showContainer = document.createElement("div");
+      showContainer.setAttribute("class", "show");
+
+      // Create the show name element
+      var showName = document.createElement("h3");
+      showName.textContent = show.name;
+      showContainer.appendChild(showName);
+
+      // Create the show image element
+      var showImage = document.createElement("img");
+      showImage.setAttribute("src", show.image.medium);
+      showContainer.appendChild(showImage);
+
+      // Append the show container to the grid
+      grid.appendChild(showContainer);
+    });
+
+    generatePagination(filteredShows.length);
+  }
+
+  //
+  //
+  //  pagination
+  function generatePagination(totalPages) {
+    // Clear the previous pagination links
+    var pagination = document.querySelector(".pagination");
+    if (pagination) {
+      pagination.parentNode.removeChild(pagination);
+    }
 
     // Create pagination links
-    var pagination = document.createElement("div");
+    pagination = document.createElement("div");
     pagination.setAttribute("class", "pagination");
     episodesContainer.appendChild(pagination);
 
@@ -249,11 +393,30 @@ document.addEventListener("DOMContentLoaded", function () {
   ////////////////// ABOVE IS MAIN DISPLAY FUNCTION ///////////////
   ////////////////////////////////////////////////////////////////
 
-  // Event listener for search bar input
+  // Event listener for search bar input for EPISODES
   searchBar.addEventListener("input", function () {
     displayEpisodes(allEpisodes);
   });
 
+  // Event listener for search bar input for SHOWS
+  searchBar.addEventListener("input", function () {
+    displayShows(allShows);
+  });
+
+  // // Event listener for search bar input for SHOWS
+  // searchBar.addEventListener("input", function () {
+  //   displayShows(allShows);
+  // });
+
+  // Fetch shows and display them
+  fetchShows()
+    .then(function (shows) {
+      displayShows(shows);
+    })
+    .catch(function (error) {
+      console.error("Error fetching shows:", error);
+    });
+
   // Display all episodes initially
-  displayEpisodes(allEpisodes);
+  // displayEpisodes(allEpisodes);
 });
